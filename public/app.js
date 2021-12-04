632b90d11cd590f2e3744f4a94064544d73c32be
Robert Drabant <rdrabant@resultsdelivered.com>
2021-12-04 11:05:50 -0800

    made changes

--- public/app.js
+++ public/app.js
@@ -1,19 +1,58 @@
 // create map
-
+// Create map:                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
 // add openstreetmap tiles
-
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 // create and main add geolocation marker
-
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 // draw the 2nd arrondissement
+// create a red polygon from an array of LatLng points
+var latlngs = [[48.863368120198004, 2.3509079846928516],
+[48.86933262048345, 2.3542531602919805],
+[48.87199261164275, 2.3400569901592183],
+[48.86993336274516, 2.3280142476578813] ,
+[48.86834104280146, 2.330308418109664],
+[48.863368120198004, 2.3509079846928516]];
 
+var polygon = L.polyline(latlngs, {color: 'green'}).addTo(myMap);                                        
+
+// zoom the map to the polygon
+myMap.fitBounds(polygon.getBounds());
 
 // create red pin marker
+var redPin = L.icon({
+    iconUrl: '/public/assets/red-pin.png',
+    iconSize: [38, 38],
+    // iconAnchor: [22, 94],
+    // popupAnchor: [-3, -3],
+    // shadowUrl: 'my-icon-shadow.png',
+    // shadowSize: [68, 95],
+    // shadowAnchor: [22, 94]
+});
 
 
-// metro station markers
+// Metro station markers:
+//const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('Réaumur-Sébastopol')
+const rS = L.marker([48.866200610611926, 2.352236247419453],{icon: redPin}).bindPopup('Réaumur-Sébastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196],{icon: redPin}).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739],{icon: redPin}).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167],{icon: redPin}).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543],{icon: redPin}).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714],{icon: redPin}).bindPopup('Grands Boulevards')
+
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
+
+
 
 
 
